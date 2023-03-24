6cab72a302f17c8d8ffab1adbf3d36d05468c562
Mayank Pradhan <nerdmyk@gmail.com>
2023-03-24 13:46:34 +0530

    4th Question

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,20 @@
+// factorial of n
+
+#include <iostream>
+using namespace std;
+
+int fact(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    return n * fact(n - 1);
+}
+
+int main()
+{
+
+    cout << fact(5);
+    return 0;
+}
