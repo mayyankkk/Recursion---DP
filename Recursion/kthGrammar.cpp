0d3beee8e5c3818d660658d6f93c80291f4d0bb6
Mayank Pradhan <nerdmyk@gmail.com>
2023-03-26 23:45:50 +0530

    10th Question

---
+++ Recursion/kthGrammar.cpp
@@ -0,0 +1,23 @@
+// Problem link: https://leetcode.com/problems/k-th-symbol-in-grammar/
+
+class Solution
+{
+public:
+    int kthGrammar(int n, int k)
+    {
+        if (n == 1 && k == 1)
+        {
+            return 0;
+        }
+        int length = 1 << (n - 1);
+        int mid = length / 2;
+        if (k <= mid)
+        {
+            return kthGrammar(n - 1, k);
+        }
+        else
+        {
+            return (~kthGrammar(n - 1, k - mid) & 1);
+        }
+    }
+};
